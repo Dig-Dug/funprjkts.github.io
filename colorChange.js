3ed94839426aa4d23d633a1edd665594a7b75dfc
unknown <dig-dug>
2021-09-01 15:05:55 +0200

    comitting

--- colorChange.js
+++ colorChange.js
@@ -1,20 +1,31 @@
 function randomColor(){
     console.log(Math.random() + "   --  sth");
+    document.getElementById("color").innerHTML = Math.random();
     return Math.random();
 }
 
-function changeColor(){
-    document.getElementById("color").style.color = "blue";
-   /* for (let i = 0; i < 10; i++) {
-     if(changeColor){
-        console.log(Math.random(i));
-        document.body.style.background = "red";
-        document.body.style.background = [i];
-        document.body.style.background = "yellow";
-      }}*/
-      randomColor();
-    document.body.style.background = "#AA0000";
-    document.body.style.background = "yellow";
-    document.body.style.background = "orange";
+const colors = ["maroon","deeppink","blueviolet","lightpink","chartreuse"];
+function changeColor() {
+  
+ // colors.forEach(colors => document.body.style.backgroundColor = colors);
+ 
+ for(i = 0; i < 5; i++){
+  //document.body.style.backgroundColor = colors[i];
+  //shift();
+  document.body.style.backgroundColor = colors.shift(i += 5);
+  //document.body.style.backgroundColor =  colors[i];
+ 
+  console.log("-> " + colors[i]); 
+  // return colors * Math.random(i)  ;
+  //document.body.style.backgroundColor = colors[Math.random() * i + i];
+  //document.body.style.background = Math.random(colors * i);
+  // randomColor(); 
+  //document.body.style.background = "#AA0000";
+  //document.body.style.background = "yellow";
+  //if(colors == "chartreuse"){changeColor(i);}
+  }  
+    //document.body.style.background = Math.random( this.colors);
+    console.log("dedww");
+    
 }
 
