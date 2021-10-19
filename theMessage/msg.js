fa9f6f844b5c3c7bfdd6f3c188053b4d1bdccfbe
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-10-19 18:21:26 +0200

    ..

--- theMessage/msg.js
+++ theMessage/msg.js
@@ -1,25 +1,25 @@
 
 
 function messageIt() {
-   console.log("dddd");
-    let fname = document.querySelector("#fname");
-    console.log(fname.value)
+  console.log("dddd");
+  let fname = document.querySelector("#fname");
+  console.log(fname.value)
 
-   if (!document.getElementById("parent")) {
-   let msg = document.createElement("p");
-   msg.innerHTML = '<p id="parent" name = "parent"> ' + fname.value  + '</p>';
-   document.getElementById("here").appendChild(msg);
+  if (!document.getElementById("parent")) {
+    let msg = document.createElement("p");
+    msg.innerHTML = '<p id="parent" name = "parent"> ' + fname.value + '</p>';
+    document.getElementById("here").appendChild(msg);
 
-   const buttonElement = document.getElementById('here');
-   buttonElement.addEventListener('click', function () {
-        console.log("kkk")
-   }) 
-  return;
+    const buttonElement = document.getElementById('here');
+    buttonElement.addEventListener('click', function () {
+      console.log("kkk")
+    })
+    return;
   };
 
-logIt();
+  logIt();
 
 
-  }
+}
 
 
