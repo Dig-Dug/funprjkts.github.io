22bc8949577827e7e454bf8fd499eb598c74295b
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-10-19 17:57:15 +0200

    commit..

--- theMessage/msg.js
+++ theMessage/msg.js
@@ -7,55 +7,19 @@ function messageIt() {
 
    if (!document.getElementById("parent")) {
    let msg = document.createElement("p");
-   msg.innerHTML = '<p id="parent" name = "parent"> ' +fname +"console.log" + '</p>';
+   msg.innerHTML = '<p id="parent" name = "parent"> ' + fname.value  + '</p>';
    document.getElementById("here").appendChild(msg);
 
    const buttonElement = document.getElementById('here');
    buttonElement.addEventListener('click', function () {
         console.log("kkk")
-   }) };
-}
+   }) 
+  return;
+  };
+
+logIt();
 
 
-/** if (!document.getElementById("parent")) {
-
-    for (var i = 0; i < items.length; i++) {
-
-      let quo = document.createElement("p");
-      let aut = document.createElement("h5");
-
-      quo.innerHTML = '<p id="parent" name = "parent"> ' + items[moth].quote + '</p>';
-      aut.innerHTML = '<h5 id="parent" name = "parent">' + items[moth].author + '</h5>';
-
-
-      console.log(items[moth].quote);
-      document.getElementById("new").appendChild(quo);
-      document.getElementById("new").appendChild(aut);
-
-      const buttonElement = document.getElementById('news-story')
-      buttonElement.addEventListener('click', function () {
-
-        console.log("ke--pasa");
-
-
-
-        let o = document.getElementById("new");
-        let d = document.getElementById("new");
-
-        while (o.firstChild) {
-          o.removeChild(o.firstChild);
-        }
-
-
-        parseURLParams()
-        return;
-
-      });
-
-
-      parseURLParams();
-
-      return;
-    }
   }
-} */
+
+
