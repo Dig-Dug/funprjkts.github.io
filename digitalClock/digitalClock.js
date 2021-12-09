346c1e09d5f5c019705c8e86736768fb23fcc3aa
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-12-09 18:42:31 +0100

    -

--- digitalClock/digitalClock.js
+++ digitalClock/digitalClock.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-
+   
 let output  = ""
     document.getElementById('date').innerHTML = output;
     console.log(output);
@@ -10,23 +10,48 @@ var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' 
 var today  = new Date();
  let hour = today.getHours();
 
+
+
 console.log(today.toLocaleDateString("de-DE", options)); // 9/17/2016
 
-   // var today = new Date();
+  
     document.getElementById('date').value += today;
     console.log(today)
     let clock = document.createElement('span');
     clock.innerHTML = '<h2 class="clock" \>'+ today.toLocaleDateString("de-DE", options); + '</h2>';
     document.getElementById("date").appendChild(clock);
     
-   /* TODO  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_datetime
+ 
+   
+  /*   let m = today.getMinutes();
+    let s = today.getSeconds();
+    
     document.getElementById('date').value += hour;
     
     let hourr = document.createElement('span');
-    hourr.innerHTML = '<h1 class="hour" \>'+ hour + '</h1>';
-    document.getElementById("date").appendChild(hourr);
-     */
-   
+    hourr.innerHTML = '<p class="hour" \>'+ hour +":"+ m +":"+ s + '</p>';
+    document.getElementById("date").appendChild(hourr); */
   
+
+    startTime();
+   
 }
+function startTime() {
+
+    var today = new Date();
+    var h = today.getHours();
+    h = h > 12 ? h - 12 : h;
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
     
