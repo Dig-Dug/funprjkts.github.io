68a9d18fc1633e4663793bcf8e54c13e04dd59d2
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-12-10 15:52:37 +0100

    -

---
+++ Calculator/calculator.js
@@ -0,0 +1,27 @@
+const number = 0;
+
+
+
+function sum(){
+   
+    let num = document.getElementById("sum").value;
+    if( num == isNaN()){
+        console.log("sssss")
+       // return false;
+    }
+    let num1 = document.getElementById("input").value;
+    
+    console.log(num , "www," )
+
+    //result()
+
+}  
+
+function result(){
+    console.log("TODO")
+}
+
+function reset(){
+    document.getElementById("erase").defaultValue = "0";
+    console.log("TODO")
+}
