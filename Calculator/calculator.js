e8cdb59782e69c2a1ea7e050a95d594d203940e5
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-12-21 13:24:38 +0100

    -

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,15 +1,49 @@
 const number = 0;
 
-
-
-function sum(){
+let num = document.getElementById("in").value;
+/* 
+function perform(){
    
-    let num = document.getElementById("sum").value;
-    if( num == isNaN()){
-        console.log("sssss")
-       // return false;
+let num = document.getElementById("in").value;
+switch(num){
+        case "+"  : sum();
+       // console.log(num + num,"++");
+        break;
+        case "-": console.log("--");
+        break;
+        case "*": console.log("**");
+        break;
+        case "/": console.log("//");
+        break;
+        case "=": console.log(")#´##=");
+        break;
+        default: console.log("TODO");
     }
-    let num1 = document.getElementById("input").value;
+   
+    } */
+   
+   
+    function sum(){
+      let num1 = document.getElementById("in").value;
+      let num2 = document.getElementById("in").value;
+        
+        console.log(num1 + num2,"++")
+        return;
+ /*  document.onclick = reset;
+   if(sum){
+        document.getElementById("erase").defaultValue = "0";
+        console.log("TOuuDO")
+       
+        return;
+    } */
+   /*  if(document.getElementById('sumo').onclick == "+"){
+      
+        console.log(num,"sssss")
+        reset();
+       
+       return false;
+    } */
+    //let num1 = document.getElementById("input").value;
     
     console.log(num , "www," )
 
