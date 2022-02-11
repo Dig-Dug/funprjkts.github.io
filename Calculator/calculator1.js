8019f01acb30a8ea6486af256e8f4d8333858c90
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-02-11 11:53:11 +0100

    -

---
+++ Calculator/calculator1.js
@@ -0,0 +1,14 @@
+let num1 = document.getElementById("num1").value;
+let num2 = document.getElementById("num2");
+
+function sume(){
+  let num1 = document.getElementById('num1').value;
+let num2 = document.getElementById('num2').value;
+ console.log(num1 += num2)
+}
+
+function reset(i){
+  document.getElementById("erase").defaultValue = "0";
+
+  console.log("reset")
+} 
