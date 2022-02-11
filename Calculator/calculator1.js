402230a6b017ab2363aa28f1a5c010cc6768c210
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-02-11 14:57:07 +0100

    -

--- Calculator/calculator1.js
+++ Calculator/calculator1.js
@@ -1,14 +1,68 @@
-let num1 = document.getElementById("num1").value;
-let num2 = document.getElementById("num2");
 
-function sume(){
+
+function sume() {
   let num1 = document.getElementById('num1').value;
-let num2 = document.getElementById('num2').value;
- console.log(num1 += num2)
+  let num2 = document.getElementById('num2').value;
+  let b = parseInt(num1);
+  let w = parseInt(num2);
+
+  console.log(b + w);
+
+  const totalResult = document.createElement("p");
+  totalResult.setAttribute("id", "total");
+  totalResult.innerHTML = b + w;
+  document.getElementById("totall").appendChild(totalResult);
+
 }
 
-function reset(i){
-  document.getElementById("erase").defaultValue = "0";
+function rest() {
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  let b = parseInt(num1);
+  let w = parseInt(num2);
 
+  console.log(b - w);
+
+  const totalResult = document.createElement("p");
+  totalResult.setAttribute("id", "total");
+  totalResult.innerHTML = b - w;
+  document.getElementById("totall").appendChild(totalResult);
+
+}
+
+function mult() {
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  let b = parseInt(num1);
+  let w = parseInt(num2);
+
+  console.log(b * w);
+
+  const totalResult = document.createElement("p");
+  totalResult.setAttribute("id", "total");
+  totalResult.innerHTML = b * w;
+  document.getElementById("totall").appendChild(totalResult);
+
+}
+function div() {
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  let b = parseInt(num1);
+  let w = parseInt(num2);
+
+  console.log(b / w);
+
+  const totalResult = document.createElement("p");
+  totalResult.setAttribute("id", "total");
+  totalResult.innerHTML = b / w;
+  document.getElementById("totall").appendChild(totalResult);
+
+}
+
+
+function reset() {
+  //document.getElementById("totall").defaultValue = "";
+  let u = document.getElementById('total');
+  u.remove();
   console.log("reset")
 } 
