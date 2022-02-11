3f5ac8e36674b5e62b38cf2363e26416c20d7dc0
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-02-11 16:57:42 +0100

    -

--- Calculator/calculator1.js
+++ Calculator/calculator1.js
@@ -1,12 +1,25 @@
+function input(e) {
+ /*  var inn = document.getElementById("numT"); */
+  
+  const num = parseInt(document.getElementById("numT").value);
 
+  //let i = parseInt(inn)
+  console.log(num)
+
+
+}
 
 function sume() {
   let num1 = document.getElementById('num1').value;
   let num2 = document.getElementById('num2').value;
+  
   let b = parseInt(num1);
   let w = parseInt(num2);
 
-  console.log(b + w);
+
+  let numT = document.getElementById('numT').value;
+  let x = parseInt(numT);
+  console.log(b + w, x);
 
   const totalResult = document.createElement("p");
   totalResult.setAttribute("id", "total");
