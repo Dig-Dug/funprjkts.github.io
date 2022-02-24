a4b968ba6a05a948d6f0ed110efcda36c9f0ea0b
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-02-24 12:18:48 +0100

    -

--- flashCards/flashCard.js
+++ flashCards/flashCard.js
@@ -15,9 +15,12 @@ function showCard(){
     d.setAttribute('class', 'titel');
 
     question.setAttribute('class', 'showCardInput');
+    question.setAttribute('id', 'questioning');
+    question.setAttribute('name', 'question');
     question.setAttribute('placeholder', 'write question here');
    
     answer.setAttribute('class', 'showCardInput');
+    answer.setAttribute('id', 'answering');
     answer.setAttribute('placeholder', ' write answer here');
 
 
@@ -31,30 +34,66 @@ function showCard(){
 
 
 
-
+   
     for(let i = 0; i < 6; i++){
        // console.log(elements);
-        document.getElementById("fc").appendChild(elements[i]);
+      document.getElementById("fc").appendChild(elements[i]);
+       }
+    
 
-    }
-
-
+   
+    return;
 
 }
 
 function closeIt(){
   let u = document.getElementById('fc');
   u.innerHTML = '';
-  console.log("reset")
+
+ 
+  console.log("reset");
+  
     console.log("close");
 }
 
 function saveIt(){
-    console.log("saave")
+   let z = document.getElementById("questioning").value;
+    console.log(z); 
+    let flash = document.createElement("div");
+    flash.setAttribute("id","flashRev");
+
+    let question = document.createElement("h3");
+    question.setAttribute("id", "questionRev");
+    question.setAttribute("onClick", "reveal()");
+    question.textContent= z; //TODO
+
+    const elements = [flash, question];
+    for(let i = 0; i < 2; i++){
+        document.getElementById("flashcards").appendChild(elements[i]);
+    }
+    console.log("saave");
+  //  showCard();
+}
+function reveal(){
+    let z = document.getElementById("answering").value;
+
+    let answer = document.createElement("h3");
+    answer.setAttribute("id", "answerRev");
+    answer.textContent= z;
+    
+    document.getElementById("flashcards").appendChild(answer);
+
+
+    console.log("wwww");
+   
+    return;
 }
 
 
-
+function deleteCards(){
+    let u = document.getElementById('flashcards');
+    u.innerHTML = '';
+  }
 
 
 //https://www.educative.io/edpresso/how-to-add-an-id-to-element-in-javascript
