aa9355253873b7a7616d49bbe865b2024f616a56
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-03-14 19:14:31 +0100

    -

---
+++ timer/timer.js
@@ -0,0 +1,11 @@
+/* const buttonElement = document.getElementById('chrono');
+
+buttonElement.addEventListener('click', function () {
+  console.log("ok")
+}) */
+
+const div = document.querySelector('div');
+
+div.addEventListener('dblclick', () => {
+	alert('You just double clicked the div!');
+});
