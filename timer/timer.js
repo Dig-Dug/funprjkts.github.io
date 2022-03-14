67ecc37f7dc7f3163b2ff629e9531231bfafdd15
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-03-14 20:53:12 +0100

    -

--- timer/timer.js
+++ timer/timer.js
@@ -1,11 +1,27 @@
-/* const buttonElement = document.getElementById('chrono');
+const buttonElement = document.getElementById('chrono');
+let t = document.getElementById('chrono').innerHTML;
 
+ let sp = t.split(" -- "); 
+let min = sp[0] 
 buttonElement.addEventListener('click', function () {
-  console.log("ok")
-}) */
 
-const div = document.querySelector('div');
 
-div.addEventListener('dblclick', () => {
-	alert('You just double clicked the div!');
-});
+  for(let i = 0; i < 60; i++){
+
+let seconds = 0;
+
+document.getElementById("chrono").innerHTML = seconds;
+  }
+  
+}) 
+
+document.getElementById("chrono").ondblclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("chrono").innerHTML = "40:00:00 :P";
+}
+
+
+window.setInterval(1000);
+//https://stackoverflow.com/questions/41711744/how-do-you-refresh-inner-html-every-second
+//https://www.freecodecamp.org/news/javascript-settimeout-js-timer-to-delay-n-seconds/
