3a1e8bccbf67338c07ac84c2704a798b72fe1e85
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-03-14 21:13:48 +0100

    -

--- timer/timer.js
+++ timer/timer.js
@@ -1,27 +1,33 @@
-const buttonElement = document.getElementById('chrono');
-let t = document.getElementById('chrono').innerHTML;
+const counter = document.getElementById('chrono');
+const countere = document.querySelector(".chrono");
 
- let sp = t.split(" -- "); 
-let min = sp[0] 
-buttonElement.addEventListener('click', function () {
+counter.addEventListener('click', function () {
 
-
-  for(let i = 0; i < 60; i++){
-
-let seconds = 0;
-
-document.getElementById("chrono").innerHTML = seconds;
-  }
+  counter.innerHTML = "ioio";
   
+
+
 }) 
+
+let intervalID = setInterval(() => {
+  countere.innerHTML += "S---------";
+}, 100);
+setTimeout(() => {
+  clearInterval(intervalID);
+}, 10000);
+
+
+
+window.setInterval(1000);
+//https://stackoverflow.com/questions/41711744/how-do-you-refresh-inner-html-every-second
+//https://www.freecodecamp.org/news/javascript-settimeout-js-timer-to-delay-n-seconds/
+
+/* let t = document.getElementById('chrono').innerHTML
+let sp = t.split(" -- "); 
+let min = sp[0]  */
 
 document.getElementById("chrono").ondblclick = function() {myFunction()};
 
 function myFunction() {
   document.getElementById("chrono").innerHTML = "40:00:00 :P";
 }
-
-
-window.setInterval(1000);
-//https://stackoverflow.com/questions/41711744/how-do-you-refresh-inner-html-every-second
-//https://www.freecodecamp.org/news/javascript-settimeout-js-timer-to-delay-n-seconds/
