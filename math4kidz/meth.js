120d7153fb61512aa69983708d34191a7595022b
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-04-05 11:41:34 +0200

    -

--- math4kidz/meth.js
+++ math4kidz/meth.js
@@ -1,4 +1,4 @@
-
+let r = document.getElementById('operand1').value;
 function add(){
     let num1 = document.getElementById('operand1');
     let sign = document.getElementById('sign');
@@ -9,17 +9,19 @@ sign.innerHTML = '+'
 
 //https://www.educative.io/edpresso/how-to-generate-a-random-number-between-a-range-in-javascript
 
-num1.innerHTML = Math.floor(Math.random() * 20);
-num2.innerHTML = Math.floor(Math.random() * 20);
+num1.setAttribute("value", num1.innerHTML = Math.floor(Math.random() * 20));
+num2.setAttribute("value",num2.innerHTML = Math.floor(Math.random() * 20));
 //
-let r = num1.value;
-let o = num1.value;
-let result = r + o;
+
+let n1 = num1.getAttribute("value");
+let n2 = num2.getAttribute("value");
+let result = parseInt(n1) + parseInt(n2);
+let rr = result.value;
 //
 let pa1 = document.getElementById('pa1');
     let pa2 = document.getElementById('pa2');
     let pa3 = document.getElementById('pa3');
-   
+
     pa1.innerHTML = Math.floor(Math.random() * 20);
     pa1.setAttribute("onclick", "test()");
     pa2.innerHTML = Math.floor(Math.random() * 20);
@@ -27,7 +29,7 @@ let pa1 = document.getElementById('pa1');
     pa3.innerHTML = Math.floor(Math.random() * 20);
     pa3.setAttribute("onclick", "test()");
 
-    console.log("55");
+    console.log("hier-->", num1, num2, result);
     return result;
 }
 
@@ -37,7 +39,7 @@ console.log(tor);
 function test(io){
 p = pa1; 
 q = pa2; 
-r = pa3; 
+r = result; 
 
 io = tor;
 
@@ -52,4 +54,4 @@ function nope(){
 
 /* let msg = document.createElement("p");
 msg.innerHTML = '<p id="parent" name = "parent"> ' + fname.value + '</p>';
-document.getElementById("here").appendChild(msg); */
+document.getElementById("here").appendChild(msg); */ 
