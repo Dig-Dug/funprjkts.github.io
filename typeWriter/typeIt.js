efe1583ac1024ed41f4f6328f0413bdbdc53ce38
unknown <40063087+Dig-Dug@users.noreply.github.com>
2022-08-30 11:25:44 +0200

    -

--- typeWriter/typeIt.js
+++ typeWriter/typeIt.js
@@ -1,8 +1,58 @@
+
+window.addEventListener('load', (e) =>{
+    show();
+})
+
+
 let typeIt = () => {
-fetch("https://type.fit/api/quotes")
+ fetch("https://type.fit/api/quotes")
   .then(function(response) {
-    return response.json();
+  return response.json();
   })
   .then(function(data) {
-    console.log(data);
-  });}
+    console.log(data[0]);
+   
+   let ran =  Math.floor(Math.random() * 200);
+   console.log(ran);
+   //JSON.stringify(theObject)
+   let u = []
+    u = JSON.stringify(data[ran]);
+    
+   for(let i = 0; i < 2; i++){
+    let r  = document.createElement('p');
+    //r.setAttribute('class','writer');
+    r.innerHTML = u;
+    let z = document.getElementById('quote').appendChild(r);
+    return;
+ }
+ }
+  );
+}
+
+
+function show(){
+
+/*  let st = 'Lorem ipsum typing effect!'
+    let s = 0, speed = 50;
+    if(s < st.length){
+
+        let r = document.createElement('p');
+        z = document.getElementById("quote").appendChild(r);
+        z.innerHTML += st.charAt(s); s+=1;
+       // setTimeout(show, speed)
+        
+        for(let i = 0; i < 1; i++){
+        console.log(typeIt(i))
+      return
+    }return 
+*/
+
+  let r = document.createElement('p');
+    z = document.getElementById("quote").appendChild(r);
+    for(let i = 0; i < 1; i++){
+    console.log(typeIt(i))
+    }
+
+  }
+
+
